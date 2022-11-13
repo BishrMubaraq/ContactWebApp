250eadc56dec5c28b6b85b02c3a8e03f5af76fdd
Bishr Mubarak <bishrmubaraq@gmail.com>
2022-11-13 14:17:17 +0530

    cors error 2

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance=axios.create({
-    baseURL: 'http://localhost:5000/api/*',
+    baseURL: 'http://52.66.246.172:5000/api/',
 })
 
 export default axiosInstance
